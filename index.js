a9b3894993ee62eec1644972b7d731ed12cadda5
Gradnos <sdolaqidze@gmail.com>
2023-02-28 21:35:11 +0400

    Create addBookToLibrary function with automatic display

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 let myLibrary = [
-  new Book("John Cena Returns", "John", 320, true),
-  new Book("Mr.beast Chronicles", "Jimmy Carter", 3, false)
+  new Book("Ronn the John", "Shota", 30, true),
+  new Book("Mr.Beast Chronicles", "Jimmy Carter", 3, false)
 ];
+let bookTemplate = document.querySelector(".bookTemplate")
+const container = document.querySelector(".content");
+
 
 function Book(title, author, pages, wasRead) {
     this.title = title;
@@ -11,10 +14,37 @@ function Book(title, author, pages, wasRead) {
 }
 
 
-console.table(myLibrary);
 
 function addBookToLibrary(book) {
-  // do stuff here
   myLibrary.push(book);
+  displayBooks();
+}
+
+
+displayBooks();
+
+function displayBooks(){
+  let books = Object.values(container.children);
+  myLibrary.forEach(book => {
+    let found = false;
+    books.forEach(element => {
+      if(book.title === element.getAttribute("data-title")){
+        found = true;
+      }
+    });
+
+    if(!found){
+      displayBook(book);
+    }
+  });
+}
+
+function displayBook(book){
+  let newBook = bookTemplate.cloneNode(true);
+  newBook.classList.remove("bookTemplate");
+  newBook.querySelector(".title").innerText = book.title;
+  newBook.querySelector(".author").innerText = book.author;
+  newBook.querySelector(".pages").innerText = `${book.pages} pages`;
+  container.appendChild(newBook);
 }
 
